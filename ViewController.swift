8987af8e0c48168877be86d27a0bba5ccafea36d
shoot21 <nikitaivashenkov01@icloud.com>
2021-10-19 18:14:41 +0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewProject
+//
+//  Created by Никита Иващенков on 19/10/2021.
+//  Copyright © 2021 Никита Иващенков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
